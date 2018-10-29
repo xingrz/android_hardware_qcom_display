2fc22d5dc10f5bdd67ac2ef4531ecf269fa9ffab
Ramakant Singh <ramakant@codeaurora.org>
2018-10-29 12:41:54 +0530

    hwc2: Check and return valid error from ControlIdlePowerCollapse.
    Idle Power collapse is not supported by video mode panel.
    Change-Id: I5be0f2efcb2f3b22951968fabb74b48d76e6fb22
    CRs-Fixed: 2320046

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -2341,8 +2341,8 @@ android::status_t HWCSession::SetIdlePC(const android::Parcel *input_parcel) {
       return -EINVAL;
     }
     auto error = hwc_display_[HWC_DISPLAY_PRIMARY]->ControlIdlePowerCollapse(enable, synchronous);
-    if (error != HWC2::Error::None) {
-      return -EINVAL;
+    if (error == HWC2::Error::Unsupported) {
+      return 0;
     }
     if (!enable) {
       Refresh(HWC_DISPLAY_PRIMARY);
